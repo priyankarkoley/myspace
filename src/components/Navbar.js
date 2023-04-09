33ebd0eaf0f20e0e5abc885e1d953c3548bbb118
priyankar-laptop <priyankarkoley@gmail.com>
2023-04-09 13:28:33 +0530

    Dark Mode Button Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,30 @@ import Menu from "./Menu.js";
 import dp from "../images/dp_rounded.jpeg";
 
 export default function Navbar() {
-  const [isExpanded, setisExpanded] = useState(true);
-  let ClickHam = () => {setisExpanded(!isExpanded);};
+  const [isExpanded, setisExpanded] = useState(false);
+  const [isLightMode, setisLightMode] = useState(true);
+  let clickHam = () => {setisExpanded(!isExpanded);};
+  let toggleTheme = () => {setisLightMode(!isLightMode);};
+
   return (
     <>
       <div className="sticky top-0 ">
         {" "}
         {/*parent*/}
         {/* NAVBAR Begin*/}
-        <div className="flex w-full items-center bg-[#305fa9b4] backdrop-blur">
-          <span
-            onClick={ClickHam}
+        <div className="flex w-full items-center pt-[0.072rem] bg-[#305fa9b4] backdrop-blur">
+          <button
+            onClick={clickHam}
             className="ml-3 material-symbols-outlined sm:hidden"
           >
             {isExpanded?'expand_less':'menu'}
-          </span>
-          <div className="ml-2 pl-3 text-xl py-3 sm:ml-3 md:ml-5 lg:ml-7 xl:ml-12 2xl:text-2xl">
+          </button>
+          <div className="ml-2 pl-3 text-xl py-3 sm:ml-2 md:ml-6 lg:ml-12 xl:ml-16 2xl:ml-20 2xl:text-2xl">
             Priyankar's Page
           </div>
-          <div className="sm:hidden ml-auto mr-3 p-2">
+          
+          <button onClick={toggleTheme} class="sm:hidden ml-auto p-2 py-auto material-symbols-outlined">{isLightMode?'dark_mode':'light_mode'}</button>
+          <div className="sm:hidden mr-3 p-2">
             <img className="h-9 rounded-full" src={dp} alt="me" />
           </div>
           <div className="hidden sm:flex ml-auto mr-5 2xl:text-lg space-x-5 md:mr-10 lg:mr-12 xl:mr-20 2xl:mr-32 md:space-x-10 lg:space-x-16 2xl:space-x-24">
@@ -30,6 +35,7 @@ export default function Navbar() {
             <div className="text-center">Folio</div>
             <div className="text-center">Resume</div>
             <div className="text-center">Contact Me</div>
+            <button onClick={toggleTheme} class="ml-16 material-symbols-outlined">{isLightMode?'dark_mode':'light_mode'}</button>
           </div>
         </div>
         {/* NAVBAR End*/}
